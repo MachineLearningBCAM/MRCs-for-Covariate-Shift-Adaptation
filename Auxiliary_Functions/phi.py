33067f8cabf61c08a2cde121e51592b25b8d4b90
joseseg <73589759+joseseg@users.noreply.github.com>
2023-03-30 16:50:46 +0200

    xd

---
+++ Auxiliary_Functions/phi.py
@@ -0,0 +1,49 @@
+import numpy as np
+from e import e
+
+def phi(Mdl, x, y):
+    if Mdl.feature_map == 'linear':
+        # Linear Kernel
+        linear = x
+        if Mdl.bias == 'bias':
+            map = np.kron(e(y, Mdl.labels), np.concatenate(([1], linear)))
+        if Mdl.bias == 'no_bias':
+            map = np.kron(e(y, Mdl.labels), linear)
+
+    elif Mdl.feature_map == 'polinom':
+        # Polynomial Kernel (degree 2)
+        pol = np.concatenate((x, x ** 2), axis=1)
+        if Mdl.bias == 'bias':
+            map = np.kron(e(y, Mdl.labels), np.concatenate(([1], pol), axis=1))
+        if Mdl.bias == 'no_bias':
+            map = np.kron(e(y, Mdl.labels), pol)
+
+    elif Mdl.feature_map == 'polinom3':
+        # Polynomial Kernel (degree 3)
+        pol = np.concatenate((x, x ** 2, x ** 3), axis=1)
+        if Mdl.bias == 'bias':
+            map = np.kron(e(y, Mdl.labels), np.concatenate(([1], pol), axis=1))
+        if Mdl.bias == 'no_bias':
+            map = np.kron(e(y, Mdl.labels), pol)
+
+    elif Mdl.feature_map == 'random':
+        # Random Feature
+        random_cos = []
+        random_sin = []
+        for i in range(Mdl.u.shape[1]):
+            random_cos = np.concatenate((random_cos, np.cos(np.dot(x, Mdl.u[:, i]))), axis=1)
+            random_sin = np.concatenate((random_sin, np.sin(np.dot(x, Mdl.u[:, i]))), axis=1)
+        random = np.sqrt(1 / Mdl.u.shape[1]) * np.concatenate((random_cos, random_sin), axis=1)
+        if Mdl.bias == 'bias':
+            map = np.kron(e(y, Mdl.labels), np.concatenate(([1], random), axis=1))
+        if Mdl.bias == 'no_bias':
+            map = np.kron(e(y, Mdl.labels), random)
+
+    elif Mdl.feature_map == 'indicatriz':
+        indicatriz = np.concatenate((x[:, 1] * (x[:, 0] >= 0), x[:, 1] * (x[:, 0] < 0)), axis=1)
+        if Mdl.bias == 'bias':
+            map = np.kron(e(y, Mdl.labels), np.concatenate(([1], indicatriz), axis=1))
+        if Mdl.bias == 'no_bias':
+            map = np.kron(e(y, Mdl.labels), indicatriz)
+
+    return map
