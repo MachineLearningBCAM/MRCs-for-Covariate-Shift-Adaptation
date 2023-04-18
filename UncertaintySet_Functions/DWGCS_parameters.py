f13657eb6dc237a51b2c9481949b7563db2e1255
joseseg <73589759+joseseg@users.noreply.github.com>
2023-04-18 10:38:29 +0200

    :)

--- UncertaintySet_Functions/DWGCS_parameters.py
+++ UncertaintySet_Functions/DWGCS_parameters.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cvxpy as cp
 
-def DWGCS_parameter(Mdl,xtr,ytr,xte):
+def DWGCS_parameters(Mdl,xtr,ytr,xte):
 
     auxtau = []
     n = xtr.shape[0]
-    t = xts.shape[0]
+    t = xte.shape[0]
 
     for i in range(n):
         auxtau.append(Mdl.beta[i] * phi(Mdl,xtr[i, :],ytr[i]))
