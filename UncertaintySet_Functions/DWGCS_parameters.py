a7018aa88fb96086893a40d2242fd3f110eae12f
joseseg <73589759+joseseg@users.noreply.github.com>
2023-04-18 13:48:40 +0200

    :)

--- UncertaintySet_Functions/DWGCS_parameters.py
+++ UncertaintySet_Functions/DWGCS_parameters.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cvxpy as cp
+import cvxpy as cvx
 from Auxiliary_Functions.phi import phi
 
 def DWGCS_parameters(Mdl,xtr,ytr,xte):
@@ -9,6 +9,31 @@ def DWGCS_parameters(Mdl,xtr,ytr,xte):
     t = xte.shape[0]
 
     for i in range(n):
-        auxtau.append(Mdl.beta[i] * phi(Mdl,xtr[i, :],ytr[i]))
-    Mdl.tau = np.mean(np.array(auxtau), axis=0) 
+        auxtau.append(Mdl.beta_[i] * phi(Mdl,xtr[i, :],ytr[i]))
+    Mdl.tau_ = np.mean(np.array(auxtau), axis=0) 
+
+    delta = 1e-6
+    d = Mdl.tau_.shape[1]
+
+    # Define the variables of the opt. problem
+    lambda_ = cvx.Variable((1,d))
+    p = cvx.Variable((t,Mdl.labels))
+    # Define the objetive function
+    objective = cvx.Minimize(cvx.sum(lambda_))
+    # Define the constraints
+    constraints = []
+    for i in range(t):
+        for j in range(Mdl.labels):
+            aux = p[i,j] * Mdl.alpha_[i] * phi(Mdl,xte[i,:],np.array([j+1]))
+            constraints.append(Mdl.tau_ - lambda_ + delta <= cvx.sum(aux))
+            constraints.append(cvx.sum(aux) <= Mdl.tau_ + lambda_ - delta)
+    constraints.append(lambda_ >= 0)
+    constraints.append(cvx.sum(p,axis=1) == np.ones(t)/t)
+    constraints.append(p >= 0)
+
+    problem = cvx.Problem(objective, constraints)
+    problem.solve()
+
+    Mdl.lambda_ = np.maximum(lambda_.value,0)
+
     return Mdl
--- Weight_Functions/DWKMM.py
+++ Weight_Functions/DWKMM.py
@@ -6,35 +6,36 @@ def DWKMM(Mdl,xtr,xte):
     n = xtr.shape[0]
     t = xte.shape[0]
     x = np.concatenate((xtr,xte), axis=0)
-    epsilon = 1-1/(np.sqrt(n))
+    epsilon_ = 1-1/(np.sqrt(n))
     B = 1000
     K=np.zeros((n+t,n+t))
 
     for i in range(n+t):
         K[i,i] = 0.5
         for j in range(i+1, n+t):
-            K[i,j] = np.exp(-np.linalg.norm(x[i,:]-x[j,:])**2/(2*Mdl.sigma**2))
+            K[i,j] = np.exp(-np.linalg.norm(x[i,:]-x[j,:])**2/(2*Mdl.sigma_**2))
     K = K+np.transpose(K)
     
     # Define the variables of the opt. problem
-    alpha = cvx.Variable((t,1))
-    beta = cvx.Variable((n,1))
+    alpha_ = cvx.Variable((t,1))
+    beta_ = cvx.Variable((n,1))
     # Define the objetive function
-    objective = cvx.Minimize(cvx.quad_form(cvx.vstack([beta/n, alpha/t]), K))
+    objective = cvx.Minimize(cvx.quad_form(cvx.vstack([beta_/n, alpha_/t]), K))
     # Define the constraints
     constraints = [ 
-        beta >= np.zeros((n,1)),
-        beta <= (B/np.sqrt(Mdl.D)) * np.ones((n, 1)),
-        alpha >= np.zeros((t,1)),
-        alpha <= np.ones((t,1)),
-        cvx.abs(cvx.sum(beta)/n - cvx.sum(alpha)/t) <= epsilon,
-        cvx.norm(alpha - np.ones((t,1))) <= (1-1/np.sqrt(Mdl.D)) * np.sqrt(t)
+        beta_ >= np.zeros((n,1)),
+        beta_ <= (B/np.sqrt(Mdl.D)) * np.ones((n, 1)),
+        alpha_ >= np.zeros((t,1)),
+        alpha_ <= np.ones((t,1)),
+        cvx.abs(cvx.sum(beta_)/n - cvx.sum(alpha_)/t) <= epsilon_,
+        cvx.norm(alpha_ - np.ones((t,1))) <= (1-1/np.sqrt(Mdl.D)) * np.sqrt(t)
     ]
+
     problem = cvx.Problem(objective, constraints)
     problem.solve()
 
-    Mdl.beta = beta.value
-    Mdl.alpha = alpha.value
+    Mdl.beta_ = beta_.value
+    Mdl.alpha_ = alpha_.value
     Mdl.min_DWKMM = problem.value
 
     return Mdl
