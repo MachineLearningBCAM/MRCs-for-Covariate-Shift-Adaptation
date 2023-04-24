b78ca39cdcdd7986c85dfe7d9bb34187cd73fecd
joseseg <73589759+joseseg@users.noreply.github.com>
2023-04-24 12:57:54 +0200

    (￣▽￣)/♫•*¨*•.¸¸♪

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-print('Hola mundo')
-from Auxiliary_Functions.e import e 
-import numpy as np
-y = np.array([1,3,5])
-n_clases = 5
-print(e(y,n_clases))
+print('Main file of the Covariate Shift python programs by José I. Segovia-Martín')
